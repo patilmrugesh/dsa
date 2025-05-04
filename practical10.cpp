af712bc6104306762bf2f8a39937c70e3f8f7abb
patilmrugesh <129309683+patilmrugesh@users.noreply.github.com>
2025-05-04 23:06:05 +0530

    Update practical10.cpp

--- practical10.cpp
+++ practical10.cpp
@@ -1,3 +1,6 @@
+// 10 Read the marks obtained by students of second year in an online examination of particular subject. 
+// Find out maximum and minimum marks obtained in that subject. Use heap data structure. Analyze the algorithm
+
 #include <iostream>
 #include <vector>
 using namespace std;
